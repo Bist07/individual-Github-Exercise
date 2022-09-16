8b31b25900c117d3c7e8e5448ee57eaa360ad41f
Abhiek Bist <abhiek7@gmail.com>
2022-09-15 20:45:21 -0700

    is Prime
    Checks if the entered number is prime

--- get integer.java
+++ get integer.java
@@ -10,5 +10,26 @@ public class getInt {
         int num = reader.nextInt();
 
         System.out.println(num + " is odd");
+<<<<<<< Updated upstream
+    
+=======
+
+      
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+         
+        if (num % i == 0) {
+            flag = true;
+            break;
+            }
+        }
+          
+    if (!flag)
+        System.out.println(num + " is a prime number.");
+    else
+        System.out.println(num + " is not a prime number.");
+    
     }
+          
+>>>>>>> Stashed changes
 }
